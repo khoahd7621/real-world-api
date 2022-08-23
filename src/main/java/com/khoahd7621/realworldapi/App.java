16c9a5441f35f69fac97fed0c10b83766431703a
khoahd7621 <hoangdangkhoa7621@gmail.com>
2022-08-23 19:57:47 +0700

    init base project

---
+++ src/main/java/com/khoahd7621/realworldapi/App.java
@@ -0,0 +1,13 @@
+package com.khoahd7621.realworldapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}
---
+++ src/test/java/com/khoahd7621/realworldapi/RealWorldApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khoahd7621.realworldapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealWorldApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
